686e20c8cb01fa7913ae250a08e93dd2db19c2a9
Aykut Kardaş <aykutkrds@gmail.com>
2020-09-23 12:57:54 +0300

    Commands name update.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import argparse
-from os import path
 from terminaltables import AsciiTable
 from colorama import Fore
 
@@ -12,11 +11,11 @@ def highlight(text):
     return Fore.GREEN + text + Fore.RESET
 
 
-def enabled_conf(conf_name):
+def enable_conf(conf_name):
     conf_enabled_path = sites_enabled_path + conf_name
     conf_available_path = sites_available_path + conf_name
 
-    is_exists = path.exists(conf_enabled_path)
+    is_exists = os.path.exists(conf_enabled_path)
 
     if is_exists:
         return list_conf()
@@ -25,9 +24,9 @@ def enabled_conf(conf_name):
     list_conf()
 
 
-def disabled_conf(conf_name):
+def disable_conf(conf_name):
     conf_enabled_path = sites_enabled_path + conf_name
-    is_exists = path.exists(conf_enabled_path)
+    is_exists = os.path.exists(conf_enabled_path)
     if not is_exists:
         return list_conf()
 
@@ -58,15 +57,15 @@ def main():
     parser = argparse.ArgumentParser(
         description='nginx-cli | show and manage configure file')
     parser.add_argument('operation')
-    parser.add_argument('--name', required=False)
+    parser.add_argument('-n', '--name', required=False)
     args = parser.parse_args()
 
     if (args.operation == 'list'):
         list_conf()
-    elif (args.operation == 'enabled'):
-        enabled_conf(args.name)
-    elif (args.operation == 'disabled'):
-        disabled_conf(args.name)
+    elif (args.operation == 'enable'):
+        enable_conf(args.name)
+    elif (args.operation == 'disable'):
+        disable_conf(args.name)
 
 
 if __name__ == '__main__':
