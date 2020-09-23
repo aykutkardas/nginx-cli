d335850c4157a8a1c4d902ab1c3a6ae9e5f74997
Tamer Ağaoğlu <tamer.a@90pixel.com>
2020-09-23 23:39:26 +0300

    setup.py and click added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-import argparse
 from terminaltables import AsciiTable
 from colorama import Fore
+import click
 
 sites_available_path = '/etc/nginx/sites-available/'
 sites_enabled_path = '/etc/nginx/sites-enabled/'
@@ -44,7 +44,7 @@ def list_conf():
         current_conf = sites_available[i]
         is_enabled = current_conf in sites_enabled
 
-        if (is_enabled):
+        if is_enabled:
             table_data.append([highlight(current_conf), highlight("enable")])
         else:
             table_data.append([current_conf, ""])
@@ -53,20 +53,33 @@ def list_conf():
     print(table)
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description='nginx-cli | show and manage configure file')
-    parser.add_argument('operation')
-    parser.add_argument('-n', '--name', required=False)
-    args = parser.parse_args()
-
-    if (args.operation == 'list'):
+@click.command()
+@click.option('--enabled_path', default=sites_enabled_path,  prompt='Sites Enabled Path?',
+              type=click.Path(dir_okay=True, exists=True))
+@click.option('--available_path', default=sites_available_path, prompt='Sites Available Path?',
+              type=click.Path(dir_okay=True, exists=True))
+@click.option('--operation', default="list", prompt='Operation Type?',
+              type=click.Choice(['list', 'enable', 'disable'], case_sensitive=False))
+@click.option('--name', prompt='Operation Name?',
+              help='The person to greet.')
+def manage(enabled_path, available_path, operation, name):
+    print(enabled_path, available_path)
+    if operation == 'list':
         list_conf()
-    elif (args.operation == 'enable'):
-        enable_conf(args.name)
-    elif (args.operation == 'disable'):
-        disable_conf(args.name)
+    elif operation == 'enable':
+        enable_conf(name)
+    elif operation == 'disable':
+        disable_conf(name)
 
 
 if __name__ == '__main__':
-    main()
+    click.echo(Fore.LIGHTGREEN_EX + """
+                   _                           _  _ 
+     _ __    __ _ (_) _ __  __  __        ___ | |(_)
+    | '_ \  / _` || || '_ \ \ \/ /_____  / __|| || |
+    | | | || (_| || || | | | >  <|_____|| (__ | || |
+    |_| |_| \__, ||_||_| |_|/_/\_\       \___||_||_|
+            |___/  
+    NGINX-CLI                           AYKUT KARDAS
+       """ + Fore.RESET)
+    manage()
