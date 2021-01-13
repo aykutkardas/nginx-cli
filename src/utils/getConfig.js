dcd30c581bdbb93aeaa6460e609f66a1d048adc2
Aykut Kardaş <aykutkrds@gmail.com>
2021-01-13 23:02:48 +0300

    getConfig utils refactored.

--- src/utils/getConfig.js
+++ src/utils/getConfig.js
@@ -1,20 +1,14 @@
-const fs = require("fs");
+const readFile = require("./readFile");
+const { paths } = require("../constant");
 
-module.exports = async function getConfig() {
-  const configPath = "/etc/.ngx";
-  const error =
-    "Please init ngx. Follow this command:".red + "\n\n 'sudo ngx init'";
+async function getConfig() {
+  const config = await readFile(paths.config);
 
-  try {
-    const isExist = await fs.existsSync(configPath);
-
-    if (isExist) {
-      const file = await fs.readFileSync("/etc/.ngx", "utf8");
-      return JSON.parse(file);
-    }
-    console.log(error);
-  } catch (err) {
-    console.log(error);
-    return null;
+  if (config) {
+    return JSON.parse(config);
   }
-};
+
+  return null;
+}
+
+module.exports = getConfig;
