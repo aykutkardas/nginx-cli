9f5404b7783dc3510c341b2691a12b18fd641ffd
Aykut Kardaş <aykutkrds@gmail.com>
2021-01-13 23:02:15 +0300

    isExist utils added.

---
+++ src/utils/isExist.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const { errors } = require("../constant");
+
+async function isExist(path) {
+  const isExist = await fs.existsSync(path);
+  if (isExist) {
+    return true;
+  } else {
+    console.log(errors.nonExist);
+    return false;
+  }
+}
+
+module.exports = isExist;
