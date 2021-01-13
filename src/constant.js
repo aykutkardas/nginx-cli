7f96ddd65c2868f710a132b77d3841a60acfad5f
Aykut Kardaş <aykutkrds@gmail.com>
2021-01-13 23:23:01 +0300

    constant updated.

--- src/constant.js
+++ src/constant.js
@@ -1,9 +1,13 @@
 const errors = {
   nonExist: "File or directory not found.",
+  notAccessible: "File or directory not accessible."
 };
 
 const paths = {
   config: "/etc/.ngx",
+  base: "/etc/nginx",
+  sitesAvailable: "/sites-available/",
+  sitesEnabled: "/sites-enabled/",
 };
 
 module.exports = { errors, paths };
