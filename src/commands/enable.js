87555f66f235f93eede7c713569915ec5732c83a
Aykut Kardaş <aykutkrds@gmail.com>
2021-01-16 23:44:15 +0300

    enable command snake_case parameter transform to camelCase

--- src/commands/enable.js
+++ src/commands/enable.js
@@ -2,13 +2,13 @@ const getConfig = require("../utils/getConfig");
 const link = require("../utils/link");
 const list = require("./list");
 
-module.exports = async function enable(conf_name) {
+module.exports = async function enable(confName) {
   const config = await getConfig();
   if (!config) return;
 
   const { paths } = config;
-  const currentPath = paths.sitesAvailable + conf_name;
-  const newPath = paths.sitesEnabled + conf_name;
+  const currentPath = paths.sitesAvailable + confName;
+  const newPath = paths.sitesEnabled + confName;
 
   const isLinked = await link(currentPath, newPath);
 
